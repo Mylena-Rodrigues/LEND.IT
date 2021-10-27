3a71958af0ea8b8be745f5032e1ddab8c6750899
Mylena-Rodrigues <mylenawz@yahoo.com.br>
2021-10-27 13:45:50 -0300

    Criação de rotas get (index), post (create, update) e delete (delete)

--- routes/emprestimosRouter.js
+++ routes/emprestimosRouter.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const emprestimosController = require('../controllers/usuariosController');
 
+//Listagem de Emprestimos
 router.get('/', emprestimosController.index);
+//Criação de Emprestimos
+router.post('/', emprestimosController.create);
+//Atualização de Emprestimos
+router.post('/:id', emprestimosController.update);
+//Deletando Emprestimos
+router.delete('/:id', emprestimosController.delete);
 
 module.exports = router;
