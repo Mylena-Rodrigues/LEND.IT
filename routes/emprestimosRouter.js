4781a754716a470c4c60057273e0cdfda8311b63
Mylena-Rodrigues <mylenawz@yahoo.com.br>
2021-10-27 16:17:25 -0300

    Ajuste no require do controller

--- routes/emprestimosRouter.js
+++ routes/emprestimosRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const emprestimosController = require('../controllers/usuariosController');
+const emprestimosController = require('../controllers/emprestimosControllers');
 
 //Listagem de Emprestimos
 router.get('/', emprestimosController.index);
